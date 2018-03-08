f3f31bdaa5575814c7493bd450d1154b1be60993
Eduard Urbach <e.urbach@gmail.com>
2018-03-08 20:07:23 +0100

    Improved crawler

--- Crawler.go
+++ Crawler.go
@@ -1,10 +1,22 @@
 package crawler
 
-import "time"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/aerogo/http/client"
+	"github.com/fatih/color"
+)
 
 // Crawler ...
 type Crawler struct {
-	Ticker *time.Ticker
+	userAgent string
+	ticker    *time.Ticker
+	tasks     chan *Task
+	wg        *sync.WaitGroup
 }
 
 // Task ...
@@ -13,14 +25,52 @@ type Task struct {
 	Destination string
 }
 
-// Queue queues a given URL to be downloaded to the given file destination.
+// Queue queues up a task.
 func (crawler *Crawler) Queue(task *Task) {
+	crawler.wg.Add(1)
+	crawler.tasks <- task
+}
 
+// Wait waits until all tasks have been completed.
+func (crawler *Crawler) Wait() {
+	crawler.wg.Wait()
+}
+
+// Download page contents to file
+func (crawler *Crawler) work(task *Task) {
+	response, err := client.Get(task.URL).Header("User-Agent", crawler.userAgent).End()
+
+	if err != nil {
+		fmt.Println(color.RedString(task.URL), err)
+		return
+	}
+
+	if response.StatusCode() != http.StatusOK {
+		fmt.Println(color.RedString(task.URL), response.StatusCode())
+		return
+	}
+
+	data := response.Bytes()
+	fmt.Println(color.GreenString(task.URL), len(data), "bytes")
+	ioutil.WriteFile(task.Destination, data, 0644)
 }
 
 // New ...
-func New(delayBetweenRequests time.Duration) *Crawler {
-	return &Crawler{
-		Ticker: time.NewTicker(delayBetweenRequests),
+func New(userAgent string, delayBetweenRequests time.Duration, tasksBufferLength int) *Crawler {
+	crawl := &Crawler{
+		userAgent: userAgent,
+		ticker:    time.NewTicker(delayBetweenRequests),
+		tasks:     make(chan *Task, tasksBufferLength),
+		wg:        &sync.WaitGroup{},
 	}
+
+	go func() {
+		for task := range crawl.tasks {
+			crawl.work(task)
+			crawl.wg.Done()
+			<-crawl.ticker.C
+		}
+	}()
+
+	return crawl
 }
